8d743e1135251a1577644b68691cc6c07b888d34
olayinka <olayinka@obmondo.com>
2024-08-27 20:13:03 +0100

    updates
    Signed-off-by: olayinka <olayinka@obmondo.com>

--- main.c
+++ main.c
@@ -15,6 +15,9 @@ struct s_block {
         char        data[1]; /* mimic dynamic-sized */
 };
 
+
+void *base = NULL;
+
 /**
  * fblk - finds a free block that may be allocated
  * @last: last allocated block, kept to easily extend the heap if no suitable block is found
@@ -39,8 +42,9 @@ t_block find_blk(t_block *last, size_t s)
  * extend_heap - extends the breakpoint of the heap
  * @last: last allocated block, to extend the heap
  * 
+ * Return: the next allocated block where extension begins
 */
-t_block extend_heap(t_block *last, size_t s)
+t_block extend_heap(t_block last, size_t s)
 {
         // empty heap
         t_block new;
@@ -63,11 +67,13 @@ t_block extend_heap(t_block *last, size_t s)
 
 
 /*
- * splblk- splits a free block if wide enough to hold new allocation plus a new block(atleast BLOCKSIZE+4).
+ * splblk - splits a free block if wide enough to hold new allocation plus a new block(atleast BLOCKSIZE+4).
  * @b: free block to be split
  * @s: size of the new allocation
+ *
+ * Return: void
 */
-void split_blk(t_block *b, size_t s)
+void split_blk(t_block b, size_t s)
 {
         t_block free;
 
@@ -80,6 +86,35 @@ void split_blk(t_block *b, size_t s)
         b->size = s;
         b->next = free;
         b->free = 1;
+}
+
+
+/*
+ * malloc - allocates memory block of specified size. it aligns size and finds a block for the allocation or extends heap.
+ * @s: the desired size
+ *
+ * Return: pointer to the allocated memory or NULL if it fails
+*/
+void *malloc(size_t s)
+{
+        // allocated block
+        t_block b;
+        t_block last;
+        size_t s_aligned;
+
+        s_aligned = ALIGN4(s);
+        if (base == NULL)
+        {
+                return extend_heap(NULL, s_aligned);
+        }
+
+        t_block fb = findblk(&last, s_aligned + BLOCK_SIZE + 4);
+        if (fb == NULL)
+        {
+                return extend_heap(last, s_aligned);
+        }
+
+        splitblk(fb,s);
 }
 
 int main(int argc, char *argv[])
